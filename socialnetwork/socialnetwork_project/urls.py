8719da03ec93e5a3ae05c85976639bcb83996fc0
Mazzart <am.magicsmile@gmail.com>
2020-01-24 23:18:51 +0200

    Added endpoints for JWT authentication, login, logout, password reset, registration

--- socialnetwork/socialnetwork_project/urls.py
+++ socialnetwork/socialnetwork_project/urls.py
@@ -15,9 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('posts.urls')),
     path('api-auth/', include('rest_framework.urls')),
+    path('api/v1/token/', TokenObtainPairView.as_view()),
+    path('api/v1/token/refresh', TokenRefreshView.as_view()),
+    path('api/v1/rest-auth/', include('rest_auth.urls')),
+    path('api/v1/rest-auth/registration/', include('rest_auth.registration.urls')),
 ]
