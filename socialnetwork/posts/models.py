5a80f7ad234cea6d0ee343195c148b1795a8550b
Mazzart <am.magicsmile@gmail.com>
2020-01-24 17:04:16 +0200

    Added Post and Like models

--- socialnetwork/posts/models.py
+++ socialnetwork/posts/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    likes = models.PositiveIntegerField(default=0)
+    dislikes = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    value = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user}:{self.post}:{self.value}'
+
+    class Meta:
+        unique_together = ('user', 'post', 'value')
