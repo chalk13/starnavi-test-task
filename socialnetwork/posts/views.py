857560727d7fa6d040a6da0846669d5dca742b4d
Mazzart <am.magicsmile@gmail.com>
2020-01-25 22:05:32 +0200

    Added PostLike, PostUnlike views

--- socialnetwork/posts/views.py
+++ socialnetwork/posts/views.py
@@ -1,7 +1,11 @@
+from django.shortcuts import get_object_or_404
+from django.contrib.auth import get_user_model
+from django.db import IntegrityError
 from rest_framework import generics
+from rest_framework.response import Response
 
-from .models import Post
-from .serializers import PostSerializer
+from .models import Post, Like
+from .serializers import PostSerializer, LikeSerializer
 from .permissions import IsAuthorOrReadOnly
 
 
@@ -14,3 +18,36 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+
+class PostLike(generics.RetrieveUpdateAPIView):
+    queryset = Like.objects.all()
+    serializer_class = LikeSerializer
+
+    def put(self, request, pk):
+        post = get_object_or_404(Post, id=pk)
+        user = get_object_or_404(get_user_model(), username=request.user)
+        try:
+            Like.objects.create(user=user, post=post, post_like=True)
+        except IntegrityError:
+            return Response(status=200, data={'msg': 'Post already liked'})
+        post.likes += 1
+        post.save()
+        return Response(status=201, data={'msg': 'Post liked'})
+
+
+class PostUnlike(generics.RetrieveDestroyAPIView):
+    queryset = Like.objects.all()
+    serializer_class = LikeSerializer
+
+    def delete(self, request, pk):
+        post = get_object_or_404(Post, id=pk)
+        user = get_object_or_404(get_user_model(), username=request.user)
+        try:
+            obj = Like.objects.get(user=user, post=post)
+            obj.delete()
+            post.likes -= 1
+            post.save()
+        except Like.DoesNotExist:
+            return Response(status=200, data={'msg': 'Post not liked'})
+        return Response(status=204)
