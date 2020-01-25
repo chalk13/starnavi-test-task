a40dfc37695773e99ad03bec97e9bc0541843c68
Mazzart <am.magicsmile@gmail.com>
2020-01-25 22:05:02 +0200

    Added endpoints for post like/unlike

--- socialnetwork/posts/urls.py
+++ socialnetwork/posts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import PostList, PostDetail
+from .views import PostList, PostDetail, PostLike, PostUnlike
 
 urlpatterns = [
     path('posts/', PostList.as_view()),
     path('posts/<int:pk>/', PostDetail.as_view()),
+    path('posts/<int:pk>/like/', PostLike.as_view()),
+    path('posts/<int:pk>/unlike/', PostUnlike.as_view()),
 ]
