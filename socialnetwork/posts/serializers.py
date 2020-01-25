0d8ae44d1db53bd6f4f71c15807b00877d39205e
Mazzart <am.magicsmile@gmail.com>
2020-01-25 22:04:20 +0200

    Added LikeSerializer and fixes for PostSerializer

--- socialnetwork/posts/serializers.py
+++ socialnetwork/posts/serializers.py
@@ -1,11 +1,18 @@
 from rest_framework import serializers
 
-from .models import Post
+from .models import Post, Like
 
 
 class PostSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Post
-        fields = ('id', 'title', 'body', 'author', 'created_at',
-                  'likes', 'dislikes',)
+        fields = ('id', 'title', 'body', 'author', 'created_at', 'likes',)
+        read_only_fields = ('likes',)
+
+
+class LikeSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Like
+        fields = ('user', 'post', 'post_like')
